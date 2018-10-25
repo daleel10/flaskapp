e4391dea5cf94a8869673606c7a7f76a3760c11c
Abdirahman <daleel1439@gmail.com>
2018-10-25 20:13:19 +0300

    Initial commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus a, quisquam quod qui commodi veritatis inventore sapiente, aperiam cumque ad repellat doloremque tempora cum ratione, ullam consectetur dignissimos provident non.',
+            'author': 'siraji',
+            'created_date': '10-25-2018'
+        },
+         {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus a, quisquam quod qui commodi veritatis inventore sapiente, aperiam cumque ad repellat doloremque tempora cum ratione, ullam consectetur dignissimos provident non.',
+            'author': 'daleel',
+            'created_date': '10-25-2018'
+        },
+         {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus a, quisquam quod qui commodi veritatis inventore sapiente, aperiam cumque ad repellat doloremque tempora cum ratione, ullam consectetur dignissimos provident non.',
+            'author': 'hammaam',
+            'created_date': '10-25-2018'
+        }
+        
+    ]
+    return articles
